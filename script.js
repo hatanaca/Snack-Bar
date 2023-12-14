1557a5206c37bd7c7fb863b8d3c1f1fc7eab1f44
Thiago Hatanaka <thiagohatanaka18@gmail.com>
2023-12-14 19:21:53 -0300

    commit
    commit

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert("Hello")
+let toastBox = document.getElementById("toastBox");
+let successMsg = "Successfully submitted";
+let errorMsg = "Something went wrong";
+let invalidMsg = "Invalid input, check again";
+
+function showToast(msg) {
+    let toast = document.createElement("div");
+    toast.classList.add('toast');
+    toast.innerHTML = 'Sucess';
+    toastBox.appendChild(toast);
+}
