773538ed1335b88e6a7af842b0cb295c96a20dd9
Thiago Hatanaka <thiagohatanaka18@gmail.com>
2023-12-14 21:38:52 -0300

    commit
    commit

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 let toastBox = document.getElementById("toastBox");
-let successMsg = "Successfully submitted";
-let errorMsg = "Something went wrong";
-let invalidMsg = "Invalid input, check again";
+let successMsg = '<i class="fa-solid fa-circle-check" style="color:green;"></i> Successfully submitted ';
+let errorMsg = '<i class="fa-solid fa-circle-exclamation" style="color:red;"></i>Something went wrong';
+let invalidMsg = ' <i class="fa-solid fa-triangle-exclamation" style="color:orange;"></i>"Invalid input, check again';
 
 function showToast(msg) {
     let toast = document.createElement("div");
     toast.classList.add('toast');
-    toast.innerHTML = 'Sucess';
+    toast.innerHTML = msg;
     toastBox.appendChild(toast);
+
+    setTimeout(() => {
+        toastBox.removeChild(toast);
+    }, 2000);
 }
